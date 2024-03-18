5042e03d3e98233e01ee8ab4a14182948170e994
bommareddysanjana <152767998+bommareddysanjana@users.noreply.github.com>
2024-03-18 14:14:00 +0530

    Create 155. Min Stack.py

---
+++ Problem List/155. Min Stack.py
@@ -0,0 +1,21 @@
+import sys
+class MinStack:
+
+    def __init__(self):
+        self.stack=[]
+        self.minimum=[float('inf')]
+
+    def push(self, x: int) -> None:
+        self.stack.append(x)
+        self.minimum.append(min(self.minimum[-1],x))
+        
+
+    def pop(self) -> None:
+        self.stack.pop()
+        self.minimum.pop()
+    def top(self) -> int:
+        return self.stack[-1]
+
+    def getMin(self) -> int:
+        return self.minimum[-1]
+    
